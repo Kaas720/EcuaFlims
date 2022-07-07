20f945d676b8572bd7d1460684d6721168ed2558
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2022-07-06 21:36:13 -0500

    Prueba de subida parte 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SlickCarouselComponent } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoEcuaFlims';
+  slides = [
+    {img: "https://i.blogs.es/1aad84/marvel/1366_521.jpeg"},
+    {img: "https://as01.epimg.net/meristation/imagenes/2021/03/12/reportajes/1615546576_319724_1615546812_noticia_normal.jpg"},
+    {img: "https://as01.epimg.net/meristation/imagenes/2018/05/07/noticia/1525671060_782091_1531814346_portada_normal.jpg"},
+    {img: "https://media.tycsports.com/files/2020/09/05/116790/marvel-avengers_862x485.jpg"}
+  ];
+  slideConfig = {"slidesToShow": 2, "slidesToScroll": 1, "infinite": true,"centerMode":true};
+  
+  @ViewChild('slickModal')
+  slickModal!: SlickCarouselComponent;
+
+  next() {
+    this.slickModal.slickNext();
+  }
+
+  prev() {
+    this.slickModal.slickPrev();
+  }
 }
