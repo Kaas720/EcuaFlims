6ca4d07e7717af6ea17bf8039a9e85a7a6f950f1
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2022-07-09 19:38:33 -0500

    Se quito la opcion del scroll mediante el maus al Carosel de los videos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,18 @@ export class AppComponent implements OnInit {
   constructor(private ElByClassName: ElementRef) {}
   title = 'ProyectoEcuaFlims';
   slides = [
-    {img: "https://i.blogs.es/1aad84/marvel/1366_521.jpeg"},
-    {img: "https://as01.epimg.net/meristation/imagenes/2021/03/12/reportajes/1615546576_319724_1615546812_noticia_normal.jpg"},
-    {img: "https://as01.epimg.net/meristation/imagenes/2018/05/07/noticia/1525671060_782091_1531814346_portada_normal.jpg"},
-    {img: "https://media.tycsports.com/files/2020/09/05/116790/marvel-avengers_862x485.jpg"}
+    {img: "https://i.blogs.es/1aad84/marvel/1366_521.jpeg",active:"active"},
+    {img: "https://as01.epimg.net/meristation/imagenes/2021/03/12/reportajes/1615546576_319724_1615546812_noticia_normal.jpg",active:"false"},
+    {img: "https://as01.epimg.net/meristation/imagenes/2018/05/07/noticia/1525671060_782091_1531814346_portada_normal.jpg",active:"false"},
+    {img: "https://media.tycsports.com/files/2020/09/05/116790/marvel-avengers_862x485.jpg",active:"false"}
   ];
   imgFondo = [
     {img: "https://pbs.twimg.com/media/FNU7GhdX0AIneOt.jpg:large",p:"SPIDER-MAN NO HAY HOME ",model:"#exampleModal"},
-    {img: "https://media.tycsports.com/files/2020/09/05/116790/marvel-avengers_862x485.jpg",p:"LOLa",model:"#Thor"}
+    {img: "https://archivos-cms.cinecolombia.com/images/_aliases/exhibition_embed/6/4/8/5/25846-1-esl-CO/534f53a439da-imagen_trailer_1000x510.jpg",p:"LIGHTYEAR",model:"#Thor"},
+    {img: "https://images6.alphacoders.com/123/1239922.jpg",p:"JURASSIC WORLD 3 DOMINION",model:"#Jurrasic"}
   ];
-  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1, "infinite": true,"nextArrow":false,"prevArrow":false};
+  slideConfig = {"slidesToShow": 1,"nextArrow":false,"prevArrow":false,"swipeToSlide":true,"infinite":false,"accessibility":false,"speed":500,"draggable": false};
+  slideConfigImage = {"slidesToShow": 2,"nextArrow":false,"prevArrow":false,"accessibility":false};
   @ViewChild('slickModal')
   slickModal!: SlickCarouselComponent;
   @ViewChild('videos')
