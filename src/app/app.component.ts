268b77b8fc1a716a34f65099dde9daa6cd752791
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2022-07-12 18:40:56 -0500

    Se culmino la pagina de inicio aun falta agragar los enlaces pero eso se hara cuando tengamos mas paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -121,4 +121,130 @@ export class AppComponent implements OnInit {
       }
     }
   }
+  div1=false;
+  div2=false;
+  div3=false;
+  div4=false;
+  divConectado="";
+  AbrirDiv(id_entradButtonIcono: string, DivOculto: string){
+    let id_entradButtonIconoLet = <HTMLVideoElement> document.getElementById(id_entradButtonIcono);
+    let DivOcultoLet = <HTMLVideoElement> document.getElementById(DivOculto);
+    if(id_entradButtonIconoLet!=null){
+      if(this.div1==false && this.div2==false && this.div3==false && this.div4==false){
+          DivOcultoLet.classList.replace("d-none", "d-block");
+          id_entradButtonIconoLet.classList.replace("bi-plus-circle","bi-x-circle");
+          if(id_entradButtonIcono == "Icon1"){
+            this.div1=true;
+            this.divConectado="div1";
+          }
+          if(id_entradButtonIcono == "Icon2"){
+            this.div2=true;
+            this.divConectado="div2";
+          }
+          if(id_entradButtonIcono == "Icon3"){
+            this.div3=true;
+            this.divConectado="div3";
+          }
+          if(id_entradButtonIcono == "Icon4"){
+            this.div4=true;
+            this.divConectado="div4";
+          }        
+      }
+      else{
+        let contador = 0;
+        
+        DivOcultoLet.classList.replace("d-none", "d-block");
+        id_entradButtonIconoLet.classList.replace("bi-plus-circle","bi-x-circle");
+        if(id_entradButtonIcono == "Icon1"){
+          this.div1=true;
+         }
+        if(id_entradButtonIcono == "Icon2"){
+          this.div2=true;
+         }
+        if(id_entradButtonIcono == "Icon3"){
+          this.div3=true;
+         }
+        if(id_entradButtonIcono == "Icon4"){
+          this.div4=true;
+         }
+        if(this.div1==true){
+         contador++;
+        }
+       if(this.div2==true){
+         contador++;
+        }
+       if(this.div3==true){
+         contador++;
+        }
+       if(this.div4==true){
+         contador++;
+        }
+       if(contador==1){
+        DivOcultoLet.classList.replace("d-block", "d-none");
+        id_entradButtonIconoLet.classList.replace("bi-x-circle","bi-plus-circle");
+        if(id_entradButtonIcono == "Icon1"){
+          this.div1=false;
+         }
+        if(id_entradButtonIcono == "Icon2"){
+          this.div2=false;
+         }
+        if(id_entradButtonIcono == "Icon3"){
+          this.div3=false;
+         }
+        if(id_entradButtonIcono == "Icon4"){
+          this.div4=false;
+         }
+       }  
+       else{
+        if(id_entradButtonIcono != "Icon1"){
+          if(this.div1 == true){
+            let IconoLet = <HTMLVideoElement> document.getElementById("Icon1");
+            let Div= <HTMLVideoElement> document.getElementById("1");
+            Div.classList.replace("d-block", "d-none");
+            IconoLet.classList.replace("bi-x-circle","bi-plus-circle");
+            this.div1=false;
+          }
+        }
+        if(id_entradButtonIcono != "Icon2"){
+          if(this.div2 == true){
+            let IconoLet = <HTMLVideoElement> document.getElementById("Icon2");
+            let Div= <HTMLVideoElement> document.getElementById("2");
+            Div.classList.replace("d-block", "d-none");
+            IconoLet.classList.replace("bi-x-circle","bi-plus-circle");
+            this.div2=false;
+          }
+        }
+        if(id_entradButtonIcono != "Icon3"){
+          if(this.div3 == true){
+            let IconoLet = <HTMLVideoElement> document.getElementById("Icon3");
+            let Div= <HTMLVideoElement> document.getElementById("3");
+            Div.classList.replace("d-block", "d-none");
+            IconoLet.classList.replace("bi-x-circle","bi-plus-circle");
+            this.div3=false;
+          }
+        }
+        if(id_entradButtonIcono != "Icon4"){
+          if(this.div4 == true){
+            let IconoLet = <HTMLVideoElement> document.getElementById("Icon4");
+            let Div= <HTMLVideoElement> document.getElementById("4");
+            Div.classList.replace("d-block", "d-none");
+            IconoLet.classList.replace("bi-x-circle","bi-plus-circle");
+            this.div4=false;
+          }
+        }
+       }      
+      }
+      
+      
+    }
+  }
+  cerrarDiv(){
+    let DivOcultoLet1 = <HTMLVideoElement> document.getElementById("Div1");
+    let DivOcultoLet2 = <HTMLVideoElement> document.getElementById("Div2");
+    let DivOcultoLet3 = <HTMLVideoElement> document.getElementById("Div3");
+    let DivOcultoLet4 = <HTMLVideoElement> document.getElementById("Div4");
+    if(DivOcultoLet1!=null && DivOcultoLet2!=null && DivOcultoLet3!=null && DivOcultoLet4!=null ){
+
+    }
+  }
 }
