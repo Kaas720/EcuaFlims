039b6a6b4c66d7accc01e54ada932b42ea09ddc6
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2022-07-19 21:03:33 -0500

    Se crearon nuevos conponentes con sus respectivos codigos y estilos para la pare del footer ademas se agregaron sus respectivos ruteos PARTE 2

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -24,4 +24,7 @@ export class FooterComponent implements OnInit {
   TerminoDeUso(){
     this.router.navigate(['/TerminoDeUso']);
   }
+  ContactanosClick(){
+    this.router.navigate(['/Contactanos']);
+  }
 }
